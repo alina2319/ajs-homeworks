8c4d7c6660f2e6da8c0a4073d91986888dfdb763
Alina <Alin.k4@yandex.ru>
2022-12-29 19:34:37 +0300

    first commit ads

--- classes-inheritance/src/js/__tests__/app.js
+++ classes-inheritance/src/js/__tests__/app.js
@@ -1,57 +1,81 @@
-import Character from '../js/character';
+import
+{
+  Bowerman, Swordsman, Magician, Daemon, Undead, Zombie,
+} from '../app';
 
-test('character name length test', () => {
-  expect(() => {
-    /* eslint-disable no-new */
-    new Character('1', 'Magician');
-  }).toThrow(new Error('incorrect name length'));
+test('Создание объекта со свойствами', () => {
+  const bowerman = new Bowerman('Breadly');
+  expect(bowerman).toEqual({
+    name: 'Breadly',
+    type: 'Bowerman',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  });
 });
 
-test('character type test', () => {
-  expect(() => {
-    /* eslint-disable no-new */
-    new Character('test1', 'incorrectType');
-  }).toThrow(new Error('type not found'));
+test('Создание объекта со свойствами', () => {
+  const swordsman = new Swordsman('Chris');
+  expect(swordsman).toEqual({
+    name: 'Chris',
+    type: 'Swordsman',
+    health: 100,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  });
 });
 
-test('character get name', () => {
-  const name = 'test1';
-  expect(new Character(name, 'Magician').name).toBe(name);
+test('Создание обекта со свойствами', () => {
+  const magician = new Magician('Marlin');
+  expect(magician).toEqual({
+    name: 'Marlin',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  });
 });
 
-test('character get type', () => {
-  const type = 'Magician';
-  expect(new Character('name', type).type).toBe(type);
+test('Создание объекта со свойствами', () => {
+  const daemon = new Daemon('Sandy');
+  expect(daemon).toEqual({
+    name: 'Sandy',
+    type: 'Daemon',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  });
 });
 
-test('character lvl up err', () => {
-  const e = new Character('test', 'Magician');
-  e.health = 0;
-  expect(() => {
-    e.levelUp();
-  }).toThrow(new Error('character is dead'));
+test('Создание объекта со свойствами', () => {
+  const undead = new Undead('Willy');
+  expect(undead).toEqual({
+    name: 'Willy',
+    type: 'Undead',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  });
 });
 
-test('character lvl up', () => {
-  const e = new Character('test', 'Magician');
-  e.levelUp();
-  const right = new Character('test', 'Magician');
-  right.level = 2;
-  right.attack = 12;
-  right.defense = 48;
-  right.health = 100;
-  expect(e).toEqual(right);
+test('Создание объекта со свойствами', () => {
+  const zombie = new Zombie('Ray');
+  expect(zombie).toEqual({
+    name: 'Ray',
+    type: 'Zombie',
+    health: 100,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  });
 });
 
-test('damage test health 0', () => {
-  const e = new Character('test', 'Magician');
-  const point = e.health / (1 - e.defense / 100) + 1;
-  e.damage(point);
-  expect(e.health).toBe(0);
-});
-
-test('damage test health 5', () => {
-  const e = new Character('test', 'Magician');
-  e.damage(20);
-  expect(e.health).toBe(88);
+test('Проверка корректности имени', () => {
+  const bowerman = new Bowerman('K');
+  expect(bowerman).toTHrowError(new Error('Данные некорректны'));
 });
