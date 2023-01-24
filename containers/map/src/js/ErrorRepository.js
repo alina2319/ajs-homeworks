d37f2b0d0fb184dd06d069b1c143c5b8e7c5bae7
Alina <Alin.k4@yandex.ru>
2023-01-24 19:42:09 +0300

    first commit ads

---
+++ containers/map/src/js/ErrorRepository.js
@@ -0,0 +1,12 @@
+export default class ErrorRepository {
+    constructor() {
+      this.collection = new Map();
+    }
+  
+    translate(code) {
+      if (this.collection.has(code)) {
+        return this.collection.get(code);
+      }
+      return 'Unknown error';
+    }
+  }
